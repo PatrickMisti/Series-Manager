4a2027d4311ccec3bc09e52197836ba6dcd6a1f0
Patrick <patrickmistlberger@gmial.com>
2020-11-05 20:10:27 +0100

    bottomsheet finished

--- series_manager/lib/views/BottomSheetSetLink.dart
+++ series_manager/lib/views/BottomSheetSetLink.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:series_manager/main.dart';
@@ -14,43 +16,49 @@ class BottomSheetSetLink extends StatefulWidget {
 }
 class _BottomSheetSetLink extends State<BottomSheetSetLink> {
   Size size;
-  TextEditingController _linkInput;
+  final _linkInput = TextEditingController();
 
   _BottomSheetSetLink({@required this.size});
 
+  getLinkAndSave(){
+
+  }
 
   showBottomSheetForm(){
+    var bottomSheetKey = Key('container');
     return Container(
-      height: 20,
-      padding: EdgeInsets.only(top: 30,left: 20,right: 20,bottom: 30),
-      child: Column(
-        children: [
-          Text(
-            "Bitte geben sie hier ihren Link ein!",
-            style: GoogleFonts.alegreyaSc(
-              fontSize: 20.5
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 30),
-            child: TextField(
-              controller: _linkInput,
-              maxLines: 5,
-              decoration: const InputDecoration(
-                hintText: "Beispiel 'http://serienstream.sx/serie\n/stream/the-walking-dead/'",
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(25))
-                ),
+      key: bottomSheetKey,
+        height: 20,
+        padding: EdgeInsets.only(top: 30,left: 20,right: 20,bottom: 20),
+        child: Column(
+          children: [
+            Text(
+              "Bitte geben sie hier ihren Link ein!",
+              style: GoogleFonts.alegreyaSc(
+                  fontSize: 20.5
               ),
             ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20))
-            )
-          ),
-        ],
-      ),
-    );
+            Container(
+                margin: EdgeInsets.only(top: 30),
+                child: TextField(
+                  keyboardType: TextInputType.url,
+                  controller: _linkInput,
+                  maxLines: 5,
+                  decoration: const InputDecoration(
+                    hintText: "Beispiel 'http://serienstream.sx/serie\n/stream/the-walking-dead/'",
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25))
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                )
+            ),
+          ],
+        ),
+      );
   }
 
   actionSheet(context){
@@ -60,40 +68,45 @@ class _BottomSheetSetLink extends State<BottomSheetSetLink> {
         enableDrag: true,
         backgroundColor: Colors.transparent,
         builder: (BuildContext content){
-          return Column(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  height: size.height *0.2,
-                  width: size.width,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                      color: Colors.white
-                  ),
-                  child: showBottomSheetForm()
-                ),
-              ),
-              SizedBox(height: 10),
-              FlatButton(
-                height: 50,
-                minWidth: size.width - 40,
-                onPressed: () =>{
-                  Navigator.pop(content)
-                },
-                color: Colors.white,
-                child: Text("Speichern",
-                  style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 20,
+          return Container(
+            height: size.height*0.5, // size.height*0.46
+            child: Column(
+              children: [
+                Container(
+                  child: Expanded(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      width: size.width,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                          color: Colors.white
+                      ),
+                      child: showBottomSheetForm(),
+                    ),
                   ),
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                SizedBox(height: 10),
+                FlatButton(
+                  height: 50,
+                  minWidth: size.width - 40,
+                  onPressed: () =>{
+                    getLinkAndSave(),
+                    Navigator.pop(content)
+                  },
+                  color: Colors.white,
+                  child: Text("Speichern",
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25)),
+                  ),
                 ),
-              ),
-              SizedBox(height: 5,)
-            ],
+                SizedBox(height: 5,)
+              ],
+            ),
           );
         }
     );
