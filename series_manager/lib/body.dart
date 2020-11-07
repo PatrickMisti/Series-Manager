d073ec49d1ef76d1a68ea71c26d2a2b940f8a129
Patrick <patrickmistlberger@gmial.com>
2020-11-07 16:17:38 +0100

    ui update

--- series_manager/lib/body.dart
+++ series_manager/lib/body.dart
@@ -6,54 +6,43 @@ import 'package:series_manager/data/DatabaseExtension/database-extension.dart';
 import 'package:series_manager/data/database/service.dart';
 import 'package:series_manager/data/entities/category.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:series_manager/data/entities/serie.dart';
 import 'package:series_manager/import/http-service.dart';
 import 'package:series_manager/main.dart';
 import 'package:series_manager/views/BottomSheetSetLink.dart';
 import 'package:series_manager/views/SeriesComponent.dart';
 
-class Body extends StatefulWidget{
+class Body extends StatefulWidget {
   @override
   _Body createState() => _Body();
 }
 
-class _Body extends State<Body>{
-
-  List<Category> categories;
+class _Body extends State<Body> {
   DataBaseExtension db;
   HttpService service;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     db = new DataBaseExtension();
     _insertDataToDb();
-    _getAllCategories();
   }
 
-  Future<void> _insertDataToDb() async{
-    service = new HttpService();
-    //await service.getDataSaveInDb("https://serienstream.sx/serie/stream/black-clover/");
-    var result = await db.getAll<Category>();
-    //await db.deleteAll<Category>();
-    if(result.length == 0){
+  Future<void> _insertDataToDb() async {
+    //var result = await db.getAll<Category>();
+    await db.deleteAll<Category>();
+    /*if(result.length == 0){
       var insertCategory = Service.getCategory();
       for(var item in insertCategory){
         await db.insert<Category>(item);
       }
-    }
+    }*/
   }
 
-  Future<void>_getAllCategories() async {
-    var result = await db.getAll<Category>();
-    setState(() {
-      categories = result;
-    });
-  }
-
-  Container categoryListView(Category category,Size size){
-    var items = [1,2,3,4,5,6,7,8,9,10];
+  Container categoryListView(Category category, Size size) {
+    var items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return Container(
-      height: size.height *0.35,
+      height: size.height * 0.35,
       child: Stack(
         children: [
           Positioned(
@@ -62,29 +51,34 @@ class _Body extends State<Body>{
             bottom: 5,
             child: Text(
               category.categoryEnum,
-              style: GoogleFonts.alef(
-                fontSize: 27,
-                color: primaryColor
-              ),
+              style: GoogleFonts.alef(fontSize: 27, color: primaryColor),
             ),
           ),
           Positioned(
             top: 10,
             height: 250,
             child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                height: 200,
-                width: size.width,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                    itemCount: items.length,
-                    itemBuilder: (_,int position){
-                      return SeriesComponent(category: category, size: size);
-                    }
-                ),
-              )
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                    height: 200,
+                    width: size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: FutureBuilder(
+                      future: db.getAll<Series>(),
+                      builder: (context, snapshot) {
+                        return snapshot.hasData
+                            ? ListView.builder(
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (_, int position) {
+                                  return SeriesComponent(
+                                      category: null, size: null); //todo
+                                })
+                            : Center(
+                                child: Text("No Data send Patrick mail"),
+                              );
+                      },
+                    )
+                )
             ),
           ),
         ],
@@ -93,7 +87,7 @@ class _Body extends State<Body>{
   }
 
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Stack(
       children: [
@@ -106,11 +100,19 @@ class _Body extends State<Body>{
               ),
               Container(
                 height: size.height - 50,
-                child: categories == null ? Container(child: Center(child: Text("No Elements"))):
-                ListView.builder(
-                  itemCount: categories.length,
-                  itemBuilder: (_,int position){
-                    return categoryListView(categories[position],size);
+                child: FutureBuilder(
+                  future: db.getAll<Category>(),
+                  builder: (context, snapshot) {
+                    return snapshot.hasData
+                        ? ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (_, int position) {
+                              return categoryListView(
+                                  snapshot.data[position], size);
+                            })
+                        : Center(
+                            child: CircularProgressIndicator(),
+                          );
                   },
                 ),
               ),
